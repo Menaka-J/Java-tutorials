a592b0d4ee068bcae25e893b45ba1e589b23252a
Menaka J <menakaraman4@gmail.com>
2025-10-23 21:31:07 +0530

    Add files via upload

---
+++ emc tutorial/if2.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+class if2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = scan.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = scan.nextInt();
+        if (num1 == num2) {
+            System.out.println("Both numbers are equal"); 
+        }else {
+            System.out.println("Numbers not equal");
+        }
+
+    }
+}
