0ffbbe96e91748792c39ffaee9441b3e2a048523
Menaka J <menakaraman4@gmail.com>
2025-10-27 00:39:28 +0530

    Add files via upload

---
+++ emc tutorial/ternary2.java
@@ -0,0 +1,9 @@
+
+class ternary2 {
+
+    public static void main(String[] args) {
+        boolean israin = true;
+        System.out.println((israin) ? "Take an umberlla" : "Enjoy the sunlight");
+
+    }
+}
