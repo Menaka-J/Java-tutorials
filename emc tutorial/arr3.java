65b68a2debf2d44b338085c9e1a948574837b141
Menaka J <menakaraman4@gmail.com>
2025-10-30 23:43:27 +0530

    Add files via upload

---
+++ emc tutorial/arr3.java
@@ -0,0 +1,10 @@
+
+class arr3 {
+
+    public static void main(String[] args) {
+        System.out.println("Two table:");
+        for (int i = 1; i < 11; i++) {
+            System.out.println("2*" + i + "=" + (2 * i));
+        }
+    }
+}
