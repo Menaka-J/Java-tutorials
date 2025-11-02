e19f157f4380fa93bca7ec7dd52ea43f8051937f
Menaka J <menakaraman4@gmail.com>
2025-11-02 22:57:11 +0530

    Add files via upload

---
+++ emc tutorial/while1.java
@@ -0,0 +1,12 @@
+
+class while1 {
+
+    public static void main(String[] args) {
+        //print from 1 to 10
+        int i = 1;
+        while (i < 11) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
