cd748c773beb72a4de2dea85b2d019fcc7fba588
Menaka J <menakaraman4@gmail.com>
2025-11-08 12:59:25 +0530

    Add files via upload

---
+++ emc tutorial/classobj2.java
@@ -0,0 +1,10 @@
+class classobj2{
+
+    int addition(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        classobj2 obj = new classobj2();
+        System.out.println(obj.addition(9, 12));
+    }
+}
