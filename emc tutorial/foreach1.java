80bcb59d31d5b4ba8e2c1b7cbd83cd245295fe25
Menaka J <menakaraman4@gmail.com>
2025-11-09 00:08:00 +0530

    Add files via upload

---
+++ emc tutorial/foreach1.java
@@ -0,0 +1,11 @@
+
+class foreach1 {
+
+    public static void main(String[] args) {
+        int[] arr = {56, 23, 34, 21, 78, 90};
+
+        for (int var : arr) {
+            System.out.println(var);
+        }
+    }
+}
