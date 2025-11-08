5648fe51c961b52210dc387f4149aa97447baee9
Menaka J <menakaraman4@gmail.com>
2025-11-09 00:08:11 +0530

    Add files via upload

---
+++ emc tutorial/foreach2.java
@@ -0,0 +1,11 @@
+
+class foreach2 {
+
+    public static void main(String[] args) {
+        String[] names = {"Dhoni", "Rohit", "Virat", "Jadeja", "Raina"};
+
+        for (String name : names) {
+            System.out.println("Name: " + name);
+        }
+    }
+}
