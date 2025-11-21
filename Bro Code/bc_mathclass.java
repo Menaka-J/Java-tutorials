da19f245cbe446fbb44ea253b8df18ceb6e265d0
Menaka J <menakaraman4@gmail.com>
2025-11-21 17:28:26 +0530

    Add files via upload

---
+++ Bro Code/bc_mathclass.java
@@ -0,0 +1,18 @@
+
+class bc_mathclass {
+
+    public static void main(String[] args) {
+        System.out.println("Math classes : ");
+        System.out.println("PI value = " + Math.PI);
+        System.out.println("E value = " + Math.E);
+        System.out.println("Minimum of 8 and 4 = " + Math.min(8, 4));
+        System.out.println("Maximum of 8 and 4 = " + Math.max(8, 4));
+        System.out.println("2 power 3 = " + Math.pow(2, 3));
+        System.out.println("absolute value = " + Math.abs(-7));
+        System.out.println("Rounded of 5.6 = " + Math.round(5.6));
+        System.out.println("Ceil of 5.6 = " + Math.ceil(5.6));
+        System.out.println("Floor of 5.6 = " + Math.floor(5.6));
+        System.out.println("sqrt of 36 = " + Math.sqrt(36));
+
+    }
+}
