01889852bf656fb4c8118066bcb324ec40703ee9
Menaka J <menakaraman4@gmail.com>
2026-01-03 21:29:27 +0530

    Add files via upload

---
+++ Bro Code/bc_filewriter.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+class bc_filewriter {
+
+    public static void main(String[] args) {
+        String path = "C:\\Users\\Menaka\\OneDrive\\Desktop\\sample.txt";
+        String content = """
+                        Hello
+                        This is java
+                        Java is fun""";
+
+        try (Scanner scan = new Scanner(System.in); FileWriter fw = new FileWriter(path); BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write(content);
+            System.out.println("Success");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (Exception e) {
+            System.out.println("OOPS! Something went wrong.");
+        }
+
+    }
+}
