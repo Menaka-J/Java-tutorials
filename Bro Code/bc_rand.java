0b8a0bd19f6b9b9ea9a1bf7edfe0fab79b29868f
Menaka J <menakaraman4@gmail.com>
2026-01-09 22:01:07 +0530

    c2

--- Bro Code/bc_rand.java
+++ Bro Code/bc_rand.java
@@ -7,5 +7,8 @@ class bc_rand {
         Random rand = new Random();
         boolean isHeads = rand.nextBoolean();
         System.out.println("Your choice is [heads] : " + isHeads);
+
+        int randnum = rand.nextInt(100) + 1;
+        System.out.println("Your score is :" + randnum);
     }
 }
