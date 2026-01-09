6f61a7f082d8e2be006b6aa34605bf9cf06ebb99
Menaka J <menakaraman4@gmail.com>
2026-01-09 21:43:03 +0530

    cm01

---
+++ tutorial/GUI/guiinput.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JOptionPane;
+
+public class guiinput {
+
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        JOptionPane.showMessageDialog(null, "Your name is " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age:"));
+        JOptionPane.showMessageDialog(null, "Your age is " + age);
+
+    }
+}
