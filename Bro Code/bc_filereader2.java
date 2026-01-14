e6c42fda57096e9808fb06278018954d9e7d70b5
Menaka J <menakaraman4@gmail.com>
2026-01-14 21:48:42 +0530

    co11

--- Bro Code/bc_filereader2.java
+++ Bro Code/bc_filereader2.java
@@ -7,23 +7,12 @@ class bc_filereader2 {
         String path = "C:\\Users\\Menaka\\OneDrive\\Desktop\\sample.txt";
         int content;
 
-        // try (FileReader fr = new FileReader(path); BufferedReader br = new BufferedReader(fr);) {
-        //     content = br.readLine();
-        //     while (content != null) {
-        //         System.out.println(content);
-        //         content = br.readLine();
-        //     }
-        // } catch (Exception e) {
-        //     System.out.println("OOPS! Something went wrong");
-        // } finally {
-        //     System.out.println("Program ended");
-        // }
         try (FileReader fr = new FileReader(path);) {
 
             content = fr.read(); //read ascii of char in file
 
             while (content != -1) {
-                System.out.println(Integer.toString(content));
+                System.out.println((char)content);
                 content = fr.read();
             }
         } catch (Exception e) {
