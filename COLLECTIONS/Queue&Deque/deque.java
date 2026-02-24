32b4971165999161afc8a25c99459c3b2e64612d
Menaka J <menakaraman4@gmail.com>
2026-02-24 22:55:53 +0530

    Add files via upload

---
+++ COLLECTIONS/Queue&Deque/deque.java
@@ -0,0 +1,24 @@
+/*
+--->>> implements ArrayDeque, LinkedList 
+--->>> insertion and removal can takes place at both front and rear[most powerful interface]
+
+for adding element
+-> add(), addAll(), offer()
+->addFirst(), addLast(), offerFirst(), offerLast()
+
+for removing element
+-> remove(), removeAll(), poll()
+-> pollFirst(), pollLast(), removeFirst(), removeLast()
+->pop() remove front
+-> removeFirstOccurence(), removeLastOccurence()
+
+for getting front
+-> peek(), element()
+->peekFirst(), peekLast(), getFirst(), getLast()
+
+size()
+isEmpty()
+contains(), containsAll()
+retainAll()
+
+*/
