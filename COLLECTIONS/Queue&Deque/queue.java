3241c38fa357e858e9b5ebe91017d11fb261a7cd
Menaka J <menakaraman4@gmail.com>
2026-03-03 20:23:40 +0530

    Add files via upload

---
+++ COLLECTIONS/Queue&Deque/queue.java
@@ -0,0 +1,18 @@
+/*
+--->>> implements ArrayDeque, LinkedList, PriorityQueue [works based on priority]
+
+for adding element
+-> add(), addAll(), offer()
+
+for removing element
+-> remove(), removeAll(), poll()
+
+for getting front
+-> peek(), element()
+
+size()
+isEmpty()
+contains(), containsAll()
+retainAll()
+
+*/
